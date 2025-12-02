7985274461d9a8eb8f8357262efb9bc91a7a7f13
Касько Юлия <y.kasko06mail.ru>
2025-12-02 23:51:17 +0300

    Подключение к базе данных

---
+++ Mukomol_Praktik/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mukomol_Praktik;
+
+public partial class Order
+{
+    public int IdOrder { get; set; }
+
+    public int IdPartner { get; set; }
+
+    public DateOnly DateOrder { get; set; }
+
+    public DateOnly? DateShipping { get; set; }
+
+    public string StatusOrder { get; set; } = null!;
+
+    public string? CommentOrder { get; set; }
+
+    public virtual Partner IdPartnerNavigation { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ Mukomol_Praktik/Models/Pastum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mukomol_Praktik.Models;
+
+public partial class Pastum
+{
+    public int IdPasta { get; set; }
+
+    public string TypePasta { get; set; } = null!;
+
+    public string? Brand { get; set; }
+
+    public int? Packaging { get; set; }
+
+    public string? ImagePasta { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
